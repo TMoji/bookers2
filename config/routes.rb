9ad71d7db714bd8a2994c231ae08d6dda41379ea
EC2 Default User <105029527+TMoji@users.noreply.github.com>
2022-07-19 18:58:26 +0000

    投稿詳細画面とリンクの設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   root to: 'homes#about'
   devise_for :users
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new,:create, :index, :show]
+  resources :users, only: [:show, :edit]
   get "/homes/about"=>"homes#about",as: "about"
 end
